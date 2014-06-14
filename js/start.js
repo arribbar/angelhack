9334675b5df2e6692f9d012c08bec51dd6a1dbb2
Flavius Popescu <flavius.popescu@gmail.com>
2014-06-14 16:38:15 +0100

    can subscribe and add markers with pub crawls

--- js/start.js
+++ js/start.js
@@ -1,4 +1,56 @@
+var MAIN_CHANNEL = 'london_pub_crawls'
 
-function publish_new_crawl(lat, long, name, description, organizer) {
+var mapOptions = {
+  center: new google.maps.LatLng(51.5, 0),
+  zoom: 7
+};
+
+var map = new google.maps.Map(document.getElementById("map-canvas"),
+    mapOptions);
+
+
+var pubnub = PUBNUB.init({
+    publish_key   : 'demo',
+    subscribe_key : 'demo'
+})
+
+
+// returns an event id (used as channel name)
+function create_pub_crawl(lat, long, crawl_name, description, organizer) {
+	pubnub.publish( {
+		channel : MAIN_CHANNEL,
+		message : { 
+			'latitude' : lat,
+			'longitude' : long,
+			'crawl_name' : crawl_name,
+			'description' : description,
+			'organizer' : organizer
+		}
+	});
 	
 }
+
+
+pubnub.subscribe( {
+	channel : MAIN_CHANNEL,
+	message : function(m) { add_pub_crawl(m) }
+});
+
+function add_pub_crawl(message) {
+	console.log("Received message, adding to map..");
+	lat = message.latitude
+	long = message.longitude
+	crawl_name = message.crawl_name
+	desc = message.description
+	org = message.organizer
+	
+	var place = new google.maps.LatLng(lat, long);
+	var marker = new google.maps.Marker({
+		position : place,
+		map: map,
+		title: crawl_name + "\n" + desc + ". \n\nBy " + org,
+		animation : google.maps.Animation.DROP
+	})
+}
+
+
