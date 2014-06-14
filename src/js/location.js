75ff52db65d317e6538fd2c180e5e85534d27430
arribbar <bartychou@gmail.com>
2014-06-14 20:58:32 +0100

    Location almost working

--- src/js/location.js
+++ src/js/location.js
@@ -1,12 +1,38 @@
-if (navigator.geolocation) {
-                     
-// Get the user's current position
-navigator.geolocation.getCurrentPosition(showPosition, showError, optn);
-} else {
-    alert('Geolocation is not supported in your browser');
-}
+
 
 function showPosition(position) {
-	alert(position)
+	alert("the position is latitude " + position.coords.latitude + " lontitude " + position.coords.longitude)
+	return position.coords ;
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
+
+function getPosition(){
+	if (navigator.geolocation) {
+	    var optn = {
+	            enableHighAccuracy : true,
+	            timeout : Infinity,
+	            maximumAge : 0
+	        };                 
+	// Get the user's current position
+	navigator.geolocation.getCurrentPosition(showPosition, showError, optn);
+	} else {
+	    alert('Geolocation is not supported in your browser');
+	}
 }
 
