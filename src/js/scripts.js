ff02ab7349626ecd8cbb8bd2fdead132d63d09a1
arribbar <bartychou@gmail.com>
2014-06-15 12:21:05 +0100

    adding part of the itinary

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,7 +11,7 @@ getPosition(function(position){
 	user_current_position.latitude = position.coords.latitude;
 	user_current_position.longitude = position.coords.longitude;
 	setUserPositionMap();
-});
+});	
 
 // returns an event id (used as channel name)
 function create_pub_crawl(lat, long, crawl_name, description, organizer) {
@@ -51,7 +51,7 @@ function add_pub_crawl(message) {
 			map: map,
 			title: crawl_name + "\n" + desc + ". \n\nBy " + org,
 			animation : google.maps.Animation.DROP,
-			icon : "../img/bar.png"
+			icon : "/src/img/bar.png"
 		})
 
 		markers_dict[crawl_name] = marker;
